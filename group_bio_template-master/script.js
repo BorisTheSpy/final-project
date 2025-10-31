9801081c1d37ed69428694fbe684da2f508bd727
Jake <vader.waltz@gmail.com>
2025-10-31 19:35:17 -0400

    feat: add Jake's bio to webpage

--- group_bio_template-master/script.js
+++ group_bio_template-master/script.js
@@ -18,16 +18,22 @@ function toggleBio(bioId) {
  */
 function showSection(sectionId) {
     const biosSection = document.getElementById("bios");
+    const biosButton = document.getElementById("show-bios");
     const visionSection = document.getElementById("vision");
+    const visionButton = document.getElementById("show-vision");
 
     // Display the bios section and hide the vision section
     if (sectionId === "bios") {
         biosSection.style.display = "flex";
         visionSection.style.display = "none";
+        biosButton.style.background = "#cf2b41";
+        visionButton.style.background = "#007bff";
     }
     // Display the vision section and hide the bios section
     else if (sectionId === "vision") {
         biosSection.style.display = "none";
         visionSection.style.display = "block";
+        biosButton.style.background = "#007bff";
+        visionButton.style.background = "#cf2b41";
     }
 }
